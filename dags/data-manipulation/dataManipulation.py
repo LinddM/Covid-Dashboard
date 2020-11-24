6463bc403ff27c2319af3c59875b5db1ce6872f9
BorisRendon <boris.rendon@ufm.edu>
2020-11-23 19:46:38 -0600

    Falta el mapa

--- dags/data-manipulation/dataManipulation.py
+++ dags/data-manipulation/dataManipulation.py
@@ -4,6 +4,8 @@ import numpy as np
 import plotly.express as px
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
+import pydeck as pdk
+
 
 
 confirmed = pd.read_csv("confirmed.csv")
@@ -55,66 +57,69 @@ if cols in metrics:
         confirmado_hasta_la_fecha = confirmed_melted[confirmed_melted["Country/Region"] == pais][confirmed_melted["fecha"] == fecha]
         st.text("De casos confirmados hasta la fecha : " + fecha)
         st.write(confirmado_hasta_la_fecha)
-        
-
-        #confirmado_por_pais = confirmed_melted.Select(['Country/Region' , 'fecha'])['confirmed'].sum()
         confirmado_por_pais = confirmed_melted[confirmed_melted["Country/Region"] == pais]#[confirmed_melted["fecha"] == fecha]
-
-        
-        #st.map(confirmado_por_pais)
-
         st.bar_chart(confirmado_por_pais['confirmed'])
-        #
-
-        
-        #
-
-
         st.text("total por fecha")
         st.write(confirmado_por_pais)
         
 
     elif metricstoshow == 'deaths':
-        st.title("deaths cases")
-        deaths_cases = st.slider("Number of deaths cases", 1 , int(deaths_melted["deaths"].max()))
-        #fecha = st.selectbox("Select date" , deaths_melted['fecha'].unique())
-        pais = st.selectbox("Select country to check data" , deaths_melted['Country/Region'].unique())
-
-        #confirmado_por_pais = deaths_melted.Select(['Country/Region' , 'fecha'])['deaths'].sum()
-        deahts_por_pais = deaths_melted[deaths_melted["Country/Region"] == pais]#[deaths_melted["fecha"] == fecha]
-        #st.map(deahts_por_pais)
-
-        st.bar_chart(deahts_por_pais['deaths'])
-        #
-
+         fecha = st.selectbox("Select date" , deaths_melted['fecha'].unique())
         
-        #
-
-
-
-        st.write(deahts_por_pais)
-        st.map(deaths_melted)
-       
+         pais = st.selectbox("Select country to check data" , deaths_melted['Country/Region'].unique())
+         deaths_hasta_la_fecha = deaths_melted[deaths_melted["Country/Region"] == pais][deaths_melted["fecha"] == fecha]
+         st.text("De casos deathss hasta la fecha : " + fecha)
+         st.write(deaths_hasta_la_fecha)
+         deaths_por_pais = deaths_melted[deaths_melted["Country/Region"] == pais]#[deaths_melted["fecha"] == fecha]
+         st.bar_chart(deaths_por_pais['deaths'])
+         st.text("total por fecha")
+         st.write(deaths_por_pais)
+        
     else:
 
-        st.title("recovered cases")
-        #recovered_cases = st.slider("Number of recovered cases", 1 , int(recovered_melted["recovered"].max()))
-        #fecha = st.selectbox("Select date" , recovered_melted['fecha'].unique())
-        pais = st.selectbox("Select country to check data" , recovered_melted['Country/Region'].unique())
-
-        #confirmado_por_pais = recovered_melted.Select(['Country/Region' , 'fecha'])['recovered'].sum()
-        recovered_por_pais = recovered_melted[recovered_melted["Country/Region"] == pais]#[recovered_melted["fecha"] == fecha]
-        #st.map(recovered_por_pais)
-
-        st.bar_chart(recovered_por_pais['recovered'])
-        #
-
+         st.title("recovered cases")
+         fecha = st.selectbox("Select date" , recovered_melted['fecha'].unique())
         
-        #
+         pais = st.selectbox("Select country to check data" , recovered_melted['Country/Region'].unique())
+         recovered_hasta_la_fecha = recovered_melted[recovered_melted["Country/Region"] == pais][recovered_melted["fecha"] == fecha]
+         st.text("Casos recuperados a la fecha :  : " + fecha)
+         st.write(recovered_hasta_la_fecha)
+         recovered_por_pais = recovered_melted[recovered_melted["Country/Region"] == pais]#[recovered_melted["fecha"] == fecha]
+         st.bar_chart(recovered_por_pais['recovered'])
+         st.text("total por fecha")
+         st.write(recovered_por_pais)
 
 
+         st.pydeck_chart(pdk.Deck(
+            map_style='mapbox://styles/mapbox/light-v9',
+            initial_view_state=pdk.ViewState(
+                latitude=40.73,
+                longitude=-74,
+                zoom=11,
+                pitch=50,
+            ),
+            layers=[
+                pdk.Layer(
+                    'HexagonLayer',
+                    data=recovered_melted,
+                    get_position= '[Long , Lat]',
+                    radius=200,
+                    elevation_scale=4,
+                    elevation_range=[0, 1000],
+                    pickable=True,
+                    extruded=True,
 
-        st.write(recovered_por_pais)
+                ),
+                pdk.Layer(
+                    'ScatterplotLayer',
+                    data=recovered_melted,
+                    get_position='[Long, Lat]',
+                    get_color='[200, 30, 0, 160]',
+                    get_radius=200,
+
+                ),
+            ],
+        ))
 
 
 
